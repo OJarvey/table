f0a8c87dbfdaa481506a117f0159fd890442418e
OJarvey <165787104+OJarvey@users.noreply.github.com>
2024-09-06 01:54:54 +0100

    inital commit

---
+++ myscript.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+   //$("h2").CSS("text-decoration", "underline"); //underlines all <h2> elements
+   //$("ul").CSS("border", "solid 1px #ccc"); //adds a border to all lists
+   //$("h2").addClass("underline");//underlines all <h2> elements
+   //$("ul").addClass("border");//adds a border to all lists
+   $("h2").removeClass("underline");//underlines all <h2> elements
+   $("ul").removeClass("border");//adds a border to all lists
+});
